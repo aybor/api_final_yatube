4158a999fc5c558f6e4c2c8c5873f60495b9ac87
Aybor <borisov_aleks@mail.ru>
2021-11-01 21:05:14 +0300

    после 1 ревью

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,4 +1,3 @@
-from django.shortcuts import get_object_or_404
 from posts.models import Comment, Follow, Group, Post, User
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
